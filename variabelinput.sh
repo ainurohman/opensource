bb6ead0e5de97d9abfd8aa63e0c8a4a738020c96
Muhamad Ainurohman <ainurohman24@gmail.com>
2024-11-26 15:06:43 +0700

    2024-26-11
    VARIABEL INPUTAN

---
+++ variabelinput.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+echo " CONTOH SCRIPT VARIABEL DARI INPUTAN";
+echo " PROGRAM INPUTAN";
+echo " MASUKAN NAMA MAHASISWA";
+read nama
+echo " MASUKAN NIM MAHASISWA";
+read nim
+echo "======================";
+echo "NAMA MAHASISWA ADALAH" $nama;
+echo "NIM MAHASISWA";
+echo "NIM MAHASISWA ADALAH" $nim;
