063139539339eedd53a977fc3d108b9d80af1a83
Muhamad Ainurohman <ainurohman24@gmail.com>
2024-11-26 14:36:06 +0700

    2024-11-26
    Contoh script variabel pada shell script

---
+++ variabel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Nama="Muhamad Ainurohman"
+NIM=1302008
+Saya_login_sebagai_siapa=`who`
+echo "Isi variabel pertama :" $Nama
+echo "Isi variabel kedua :" $NIM
+echo "Isi variabel ketiga :" $Saya_login_sebagai_siapa
